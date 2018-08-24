17063518e7f5bbbad308e5f662c5a815c7a0f6c9
Peter Krajnik <krajnik@optimal-systems.de>
2018-08-24 22:53:56 +0200

    new cli project + deps

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {EoCoreModule} from '@eo-sdk/core';
+import {EoClientModule} from '@eo-sdk/client';
+import {EoFrameworkCoreModule} from '@eo-sdk/client';
+import {ToastrModule} from 'ngx-toastr';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NoopAnimationsModule,
+    // temporary: extra import to provide ToastrService in production mode (https://github.com/scttcper/ngx-toastr/issues/549)
+    ToastrModule.forRoot(),
+    EoCoreModule.forRoot({
+      main: ['assets/_default/config/main.json', 'assets/config/main.json'],
+      translations: ['assets/_default/i18n/', 'assets/i18n/']
+    }),
+    EoFrameworkCoreModule.forRoot(),
+    EoClientModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {
+}
