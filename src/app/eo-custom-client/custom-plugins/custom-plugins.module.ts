9053a84f035535888fd7bc30bd2affdf29bddb2c
Peter Krajnik <krajnik@optimal-systems.de>
2018-08-27 20:13:37 +0200

    created custom agm map

--- src/app/eo-custom-client/custom-plugins/custom-plugins.module.ts
+++ src/app/eo-custom-client/custom-plugins/custom-plugins.module.ts
@@ -4,17 +4,21 @@ import {EoFrameworkModule} from '@eo-sdk/client';
 import {PluginsModule} from '@eo-sdk/client';
 import {EoPlugin} from '@eo-sdk/client';
 import {links} from '../custom-states/custom-states.module';
+import {MapComponent} from './map/map.component';
+import {AgmCoreModule} from '@agm/core';
 
 export const entryComponents: EoPlugin[] = [
+  MapComponent,
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     EoFrameworkModule,
+    AgmCoreModule.forRoot(),
     PluginsModule.forRoot(entryComponents, links)
   ],
-  declarations: [],
+  declarations: [MapComponent],
   exports: [PluginsModule]
 })
 export class CustomPluginsModule {
