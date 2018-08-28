7aefef7e93cc7fb13cf19daf2c25800a9be17677
Peter Krajnik <krajnik@optimal-systems.de>
2018-08-28 14:29:07 +0200

    version update

--- src/app/eo-custom-client/custom-states/custom-states.module.ts
+++ src/app/eo-custom-client/custom-states/custom-states.module.ts
@@ -13,7 +13,7 @@ export const routes: Route[] = [
 export const links: EoLinkPlugin[] = [
   {path: 'https://developer.enaio.org/display/DD/enaio+redline+4+client', id: 'eo.custom.link.developer.enaio', matchType: /sidebar-profile/},
   {path: 'https://developer.enaio.org/display/DD/enaio+redline+4+client', id: 'eo.custom.link.developer.enaio', matchType: /sidebar-navigation/},
-  CustomComponent,
+  CustomComponent
   // {path: '/dashboard', id: 'eo.custom.state.sample', matchType: /sidebar-navigation/, queryParams: {debug: true}},
   // {path: 'https://google.com/', id: 'eo.custom.state.sample', matchType: /sidebar-profile/}
 ];
