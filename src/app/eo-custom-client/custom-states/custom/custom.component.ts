d255e39468d303747b2de2ea9de4e1066f64488f
Frank Wille <f.wille@optimal-systems.de>
2018-08-30 20:16:55 +0200

    update custom state

--- src/app/eo-custom-client/custom-states/custom/custom.component.ts
+++ src/app/eo-custom-client/custom-states/custom/custom.component.ts
@@ -1,19 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {Chart} from 'chart.js';
+import {TranslateService, SearchService} from '@eo-sdk/core';
+import {BaseChartDirective} from 'ng2-charts';
+import {AppSearchService} from '@eo-sdk/client';
+
 
 @Component({
   selector: 'eo-custom',
   templateUrl: './custom.component.html',
   styleUrls: ['./custom.component.scss']
 })
-export class CustomComponent implements OnInit {
+export class CustomComponent {
 
   static id = 'eo.custom.state.custom';
-  static path =  'custom/custom';
+  static path = 'custom/custom';
   static matchType: RegExp = /sidebar-navigation/;
 
-  constructor() { }
+  groupCount: number;
+  totalCount: number;
+  barChart;
+  activeType = 0;
+  types = [];
 
-  ngOnInit() {
+  @ViewChild(BaseChartDirective) chartEl: BaseChartDirective;
+
+  constructor(private translate: TranslateService,
+              private searchService: SearchService,
+              private appSearchService: AppSearchService) {
+
+    this.fetchData();
   }
 
+  chartClicked(el?) {
+    if (!el || (this.activeType === this.groupCount && el.active[0])) {
+      this.activeType = el ? el.active[0]._index : this.groupCount;
+      this.barChart = this.dataToChart(this.types[this.activeType]);
+      setTimeout(() => {
+        this.chartEl.chart.config.data.labels = this.barChart.labels;
+        this.chartEl.chart.update();
+        }, 0);
+    }
+  }
+
+  dataToChart(groups: any[]): Chart {
+    return {
+      data: [{
+        data: groups.map(g => g.count),
+        label: ''
+      }],
+      labels: groups.map(g => g.label)
+    };
+  }
+
+
+  private fetchData() {
+    this.appSearchService.setTerm('');
+    this.appSearchService
+      .queryState$
+      .subscribe(res => {
+          this.totalCount = res.count;
+          this.groupCount = this.appSearchService.objectTypeGroups.length;
+          this.types = [];
+
+          const main = this.appSearchService.objectTypeGroups.map((g) => {
+            const type = g.types.map(t => ({label: t.label, count: res.aggregations.type.get(t.qname) || 0}));
+            this.types.push(type.sort((a, b) => b.count - a.count));
+            return {label: g.label === '0' ? this.translate.instant('eo.quicksearch.result.group.global') : g.label,
+              count: type.map(t => t.count).reduce((a, b) => a + b, 0)};
+          });
+
+          this.types.push(main.sort((a, b) => b.count - a.count));
+          this.chartClicked();
+        }
+      );
+  }
 }
+
