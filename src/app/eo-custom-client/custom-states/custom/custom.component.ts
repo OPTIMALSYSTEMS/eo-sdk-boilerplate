006587b9015a7c35e9bf3f1a2d9b09c2e747f5f1
Frank Wille <f.wille@optimal-systems.de>
2018-08-31 10:44:01 +0200

    unsubscribe on destroy

--- src/app/eo-custom-client/custom-states/custom/custom.component.ts
+++ src/app/eo-custom-client/custom-states/custom/custom.component.ts
@@ -2,15 +2,15 @@ import {Component, ViewChild} from '@angular/core';
 import {Chart} from 'chart.js';
 import {TranslateService, SearchService} from '@eo-sdk/core';
 import {BaseChartDirective} from 'ng2-charts';
-import {AppSearchService} from '@eo-sdk/client';
-
+import {AppSearchService, UnsubscribeOnDestroy} from '@eo-sdk/client';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'eo-custom',
   templateUrl: './custom.component.html',
   styleUrls: ['./custom.component.scss']
 })
-export class CustomComponent {
+export class CustomComponent extends UnsubscribeOnDestroy {
 
   static id = 'eo.custom.state.custom';
   static path = 'custom/custom';
@@ -27,7 +27,7 @@ export class CustomComponent {
   constructor(private translate: TranslateService,
               private searchService: SearchService,
               private appSearchService: AppSearchService) {
-
+    super();
     this.fetchData();
   }
 
@@ -38,7 +38,7 @@ export class CustomComponent {
       setTimeout(() => {
         this.chartEl.chart.config.data.labels = this.chart.labels;
         this.chartEl.chart.update();
-        }, 0);
+      }, 0);
     }
   }
 
@@ -57,6 +57,9 @@ export class CustomComponent {
     this.appSearchService.setTerm('');
     this.appSearchService
       .queryState$
+      .pipe(
+        takeUntil(this.componentDestroyed$)
+      )
       .subscribe(res => {
           this.totalCount = res.count;
           this.groupCount = this.appSearchService.objectTypeGroups.length;
@@ -65,8 +68,10 @@ export class CustomComponent {
           const main = this.appSearchService.objectTypeGroups.map((g) => {
             const type = g.types.map(t => ({label: t.label, count: res.aggregations.type.get(t.qname) || 0}));
             this.types.push(type.sort((a, b) => b.count - a.count));
-            return {label: g.label === '0' ? this.translate.instant('eo.quicksearch.result.group.global') : g.label,
-              count: type.map(t => t.count).reduce((a, b) => a + b, 0)};
+            return {
+              label: g.label === '0' ? this.translate.instant('eo.quicksearch.result.group.global') : g.label,
+              count: type.map(t => t.count).reduce((a, b) => a + b, 0)
+            };
           });
 
           this.types.push(main.sort((a, b) => b.count - a.count));
