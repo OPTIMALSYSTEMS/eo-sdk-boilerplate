0d0abf6bee054f8f4d1617be2a040b08e3e23384
Frank Wille <f.wille@optimal-systems.de>
2018-08-31 10:47:26 +0200

    unsubscribe on destroy

--- src/app/eo-custom-client/custom-plugins/map-frame/map-frame.component.ts
+++ src/app/eo-custom-client/custom-plugins/map-frame/map-frame.component.ts
@@ -9,8 +9,8 @@ import {EnaioEvent} from '@eo-sdk/core';
 import {DmsObject} from '@eo-sdk/core';
 import {DmsService} from '@eo-sdk/core';
 import {EventService} from '@eo-sdk/core';
-import {SelectionService} from '@eo-sdk/client';
-import {filter, flatMap, map} from 'rxjs/operators';
+import {SelectionService, UnsubscribeOnDestroy} from '@eo-sdk/client';
+import {filter, flatMap, map, takeUntil} from 'rxjs/operators';
 import {LocationService} from '../../services/location.service';
 
 
@@ -19,7 +19,7 @@ import {LocationService} from '../../services/location.service';
   templateUrl: './map-frame.component.html',
   styleUrls: ['./map-frame.component.scss']
 })
-export class MapFrameComponent implements AfterViewInit {
+export class MapFrameComponent extends UnsubscribeOnDestroy implements AfterViewInit {
 
   static id = 'eo.custom.plugin.map-frame';
   static matchType: RegExp = /object-details-tab.*/;
@@ -28,13 +28,20 @@ export class MapFrameComponent implements AfterViewInit {
   mapAvailable;
   @ViewChild('mapFrame') mapFrame: ElementRef;
 
-  constructor(private selectionService: SelectionService, private dmsService: DmsService,
-              private renderer: Renderer2, private eventService: EventService, private locationService: LocationService) {
+  constructor(private selectionService: SelectionService,
+              private dmsService: DmsService,
+              private renderer: Renderer2,
+              private eventService: EventService,
+              private locationService: LocationService) {
+    super();
   }
 
   private renderMap(location = {}) {
     this.locationService
       .mapsUrl(location)
+      .pipe(
+        takeUntil(this.componentDestroyed$)
+      )
       .subscribe(url => {
         this.mapAvailable = false;
         this.renderer.setAttribute(this.mapFrame.nativeElement, 'src', url);
@@ -52,10 +59,11 @@ export class MapFrameComponent implements AfterViewInit {
 
     this.selectionService.focus$
       .pipe(
+        takeUntil(this.componentDestroyed$),
         map(d => d.target || d.dmsItem || d),
         filter(d => d.id),
         flatMap(d => {
-          return this.dmsService.getDmsObject(d.id, d.typeName || d.type, d.version)
+          return this.dmsService.getDmsObject(d.id, d.typeName || d.type, d.version);
         }),
       )
       .subscribe((data: DmsObject) => this.setupMap(data), error => this.renderMap());
@@ -63,6 +71,9 @@ export class MapFrameComponent implements AfterViewInit {
 
     this.eventService
       .on(EnaioEvent.DMS_OBJECT_UPDATED)
+      .pipe(
+        takeUntil(this.componentDestroyed$)
+      )
       .subscribe(event => {
         if (this.context && this.context.id === event.data.id) {
           this.setupMap(event.data);
