b1576787b50bee1a230f839a03b7ee6541225ea0
Peter Krajnik <krajnik@optimal-systems.de>
2018-08-31 11:40:49 +0200

    regexp fixed

--- src/app/eo-custom-client/custom-states/custom-states.module.ts
+++ src/app/eo-custom-client/custom-states/custom-states.module.ts
@@ -12,11 +12,9 @@ export const routes: Route[] = [
 ];
 
 export const links: EoLinkPlugin[] = [
-  {path: 'https://developer.enaio.org/display/DD/enaio+redline+4+client', id: 'eo.custom.link.developer.enaio', matchType: /sidebar-profile/},
-  {path: 'https://developer.enaio.org/display/DD/enaio+redline+4+client', id: 'eo.custom.link.developer.enaio', matchType: /sidebar-navigation/},
+  {path: 'https://developer.enaio.org/display/DD/enaio+redline+4+client', id: 'eo.custom.link.developer.enaio', matchType: new RegExp('sidebar-profile')},
+  {path: 'https://developer.enaio.org/display/DD/enaio+redline+4+client', id: 'eo.custom.link.developer.enaio', matchType: new RegExp('sidebar-navigation')},
   CustomComponent
-  // {path: '/dashboard', id: 'eo.custom.state.sample', matchType: /sidebar-navigation/, queryParams: {debug: true}},
-  // {path: 'https://google.com/', id: 'eo.custom.state.sample', matchType: /sidebar-profile/}
 ];
 
 @NgModule({
