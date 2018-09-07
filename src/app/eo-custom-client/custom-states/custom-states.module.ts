c4ff71a73ac8f073901d0783da5545b9dd111517
Peter Krajnik <krajnik@optimal-systems.de>
2018-09-07 14:35:32 +0200

    simple list + simple preview + guard

--- src/app/eo-custom-client/custom-states/custom-states.module.ts
+++ src/app/eo-custom-client/custom-states/custom-states.module.ts
@@ -4,11 +4,18 @@ import {Route, RouterModule} from '@angular/router';
 import {EoFrameworkModule} from '@eo-sdk/client';
 import {EoLinkPlugin} from '@eo-sdk/client';
 import {AuthGuard} from '@eo-sdk/client';
+import {SimpleListComponent} from './simple-list/simple-list.component';
+import {CanDeactivateListGuard} from './simple-list/can-deactivate-list.guard';
+import {SimplePreviewComponent} from './simple-preview/simple-preview.component';
 
 export const routes: Route[] = [
+  {path: SimplePreviewComponent.path, component: SimplePreviewComponent, canActivate: [AuthGuard]},
+  {path: SimpleListComponent.path, component: SimpleListComponent, canActivate: [AuthGuard], canDeactivate: [CanDeactivateListGuard]},
 ];
 
 export const links: EoLinkPlugin[] = [
+  SimplePreviewComponent,
+  SimpleListComponent,
 ];
 
 @NgModule({
@@ -17,7 +24,7 @@ export const links: EoLinkPlugin[] = [
     EoFrameworkModule,
     RouterModule.forChild(routes)
   ],
-  declarations: []
+  declarations: [SimpleListComponent, SimplePreviewComponent]
 })
 export class CustomStatesModule {
 }
